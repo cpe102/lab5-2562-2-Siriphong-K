f7e2ef944680b8f545b8324832299e08190c8438
Siriphong-K <59278536+Siriphong-K@users.noreply.github.com>
2020-01-07 16:48:32 +0700

    Update lab5_1.cpp

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,25 +1,40 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
 	int N;
 	cout << "Enter the number of student: ";
 	cin >> N;
-	
-	string name[N];
+
+	string nam[N];
 	int age[N];
+	int i=0;
+	while(i<N)
+	{
 	cout << "Name of student = ";
+	cin >> nam[i];
 	cout << "Age of student = ";
+	cin >> age[i];
+	i++;
+	}
 	
 	int key;
-	cout << "--------------------------------------\n";
+	cout << "-------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
-	cout << "--------------------------------------\n";
-	
+	cout << "-------------------------\n";
+    int A=0;
+	while(A<N)
+	{
+	 if(age[A]==key){
+	cout<<nam[A]<<"\n";
+	 }
+	A++;
+	}
 
-	
-	cout << "--------------------------------------\n";
-	
+
+	cout << "-------------------------\n";
+
 	return 0;
 }
