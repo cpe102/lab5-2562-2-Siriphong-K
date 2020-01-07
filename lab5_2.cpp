e6754d388fc00ff5027e5026f350bd0c02d61b2e
Siriphong-K <59278536+Siriphong-K@users.noreply.github.com>
2020-01-07 16:49:21 +0700

    Update lab5_2.cpp

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,17 +1,33 @@
 #include<iostream>
 using namespace std;
-
-
+string mixText(string A,string B){
+	int a=A.size(),b=B.size();
+	if(a==b){
+		int i=0;
+		string C;
+		while(i<a){
+			C=C+A[i]+B[i];
+	
+			i++;
+		}
+		return C;
+	}	
+	else
+	{
+		 return"E";
+	}
+	
+}
 
 
 int main(){	
-	cout << mixText("AAA","BBB") << "\n";
+	cout << mixText("AAA","BBB") << "\n"; 
 	cout << mixText("Hello","World") << "\n";
 	cout << mixText("SOTUS","CHEER") << "\n";
 	cout << mixText("1234","5678") << "\n";
 	cout << mixText("6","9") << "\n";
 	cout << mixText("XXXXXXX","YYY") << "\n";	
 	cout << mixText("Y","XX") << "\n";	
-	
+
 	return 0;	
 }
